0a51d70d7a2d90bda51eafb3b61423949755c8c6
ARJUN P <arjunpchr@gmail.com>
2024-02-08 13:50:55 +0530

    user management

---
+++ Frontend/src/pages/adminDash.jsx
@@ -0,0 +1,11 @@
+
+import DataTable from "../components/adminTable"
+export const Admin = () => {
+    return (
+        <>
+            <div className="w-screen h-screen bg-gray-900 flex justify-center items-center">
+                <DataTable />
+            </div>
+        </>
+    )
+}
